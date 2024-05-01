54d7983fb0d0f4c12580ba414296967da3c05a88
Chiranjib <93111779+Koch-Dev@users.noreply.github.com>
2024-05-01 14:43:00 +0530

    Update play.py

--- play.py
+++ play.py
@@ -4,8 +4,9 @@ from pyrogram.types import Message
 from pytgcalls import idle
 from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream
+from pytgcalls.types import Update
 
-CHANNEL_ID = -1001996363416
+CHANNEL_ID = -1001952511944
 app = Client(
     'py-tgcalls',
     api_id=21309577,
@@ -14,7 +15,7 @@ app = Client(
 call_py = PyTgCalls(app)
 call_py.start()
 
-CHANNEL_ID = -1001996363416
+CHANNEL_ID = -1001952511944
 
 @app.on_message(filters.command('live') & filters.incoming & ~filters.user(5218610039))
 async def stream(client, m: Message):
@@ -25,16 +26,22 @@ async def stream(client, m: Message):
         link = m.text.split(None, 1)[1]
         huehue = await m.reply("`Trying to Play 📻`")         
         try:
-            await call_py.join_group_call(
+            await call_py.play(
                 CHANNEL_ID,
                 MediaStream(
                     link,            
                     video_flags=MediaStream.IGNORE,
                 ), 
             ) 
-            add_to_queue(chat_id, "Radio 📻", link, link, "Audio", 0)
             await huehue.edit(f"Started Playing **[Radio 📻]({link})** in `{chat_id}`", disable_web_page_preview=True)
         except Exception as ep:
             await huehue.edit(f"`{ep}`")
+
+@call_py.on_update()
+async def on_update(client: PyTgCalls, update: Update):
+    if update.event == "stream_end":
+        print("Stream ended")
+    elif update.event == "closed_voice_chat":
+        print("Voice chat closed")
 
 idle()
